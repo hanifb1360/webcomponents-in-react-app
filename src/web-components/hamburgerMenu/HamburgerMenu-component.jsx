b263e6f5a886e36de03907a1a0ddff39311d3af1
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-02-11 01:27:06 +0100

    comments

--- src/web-components/hamburgerMenu/HamburgerMenu-component.jsx
+++ src/web-components/hamburgerMenu/HamburgerMenu-component.jsx
@@ -1,32 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 const HamburgerMenu = ({ onToggle }) => {
-const menuRef = useRef(null);
-const [hidden, setHidden] = useState(false);
+  // Create a reference to the hamburger menu element
+  const menuRef = useRef(null);
 
-useEffect(() => {
-const menu = menuRef.current;
-if (!menu) {
-  return;
-}
+  // Use the state to keep track of whether the hamburger menu should be hidden or not
+  const [hidden, setHidden] = useState(false);
 
-const handleClick = () => {
-  onToggle();
-  setHidden(!hidden);
-};
+  // Use an effect to add an event listener to the hamburger menu that toggles it when clicked
+  useEffect(() => {
+    const menu = menuRef.current;
+    
+    // If the hamburger menu is not yet available, return
+    if (!menu) {
+      return;
+    }
 
-menu.addEventListener('click', handleClick);
+    // Create a function to handle the click event and toggle the hamburger menu
+    const handleClick = () => {
+      onToggle();
+      setHidden(!hidden);
+    };
 
-return () => {
-  menu.removeEventListener('click', handleClick);
-};
-}, [onToggle, hidden]);
+    // Add the click event listener to the hamburger menu
+    menu.addEventListener('click', handleClick);
 
-return (
-<hamburger-menu ref={menuRef} style={{ display: hidden ? 'none' : 'block' }}>
-☰
-</hamburger-menu>
-);
+    // Return a function to remove the click event listener when the component unmounts
+    return () => {
+      menu.removeEventListener('click', handleClick);
+    };
+  }, [onToggle, hidden]);
+
+  // Return the hamburger menu element, using the reference to pass it to React
+  // The display style is set based on the state, either hidden or not
+  return (
+    <hamburger-menu ref={menuRef} style={{ display: hidden ? 'none' : 'block' }}>
+      ☰
+    </hamburger-menu>
+  );
 };
 
 export default HamburgerMenu;
--- src/web-components/hamburgerMenu/HamburgerMenu.js
+++ src/web-components/hamburgerMenu/HamburgerMenu.js
@@ -1,8 +1,12 @@
 class HamburgerMenu extends HTMLElement {
   constructor() {
+    // Call the parent class constructor
     super();
 
+    // Attach a shadow root to this custom element
     this.attachShadow({ mode: 'open' });
+
+    // Set the initial HTML content of the shadow root
     this.shadowRoot.innerHTML = `
       <style>
         :host {
@@ -16,16 +20,26 @@ class HamburgerMenu extends HTMLElement {
       </style>
       <slot></slot>
     `;
+
+    // Set an initial value for the toggled state
     this._toggled = false;
   }
   
+  // When the custom element is added to the page
   connectedCallback() {
+    // Add a click event listener to the shadow root
     this.shadowRoot.addEventListener('click', this._toggle.bind(this));
   }
 
+  // The toggle function that changes the toggled state
   _toggle() {
+    // Reverse the toggled state
     this._toggled = !this._toggled;
+
+    // Dispatch a custom "toggle" event with bubbles set to true
     this.dispatchEvent(new CustomEvent('toggle', { bubbles: true }));
+
+    // Show or hide the element based on the toggled state
     if (this._toggled) {
       this.shadowRoot.querySelector("slot").classList.add("hide");
     } else {
@@ -34,6 +48,8 @@ class HamburgerMenu extends HTMLElement {
   }
 }
 
+// Register the custom element with the browser
 customElements.define('hamburger-menu', HamburgerMenu);
 
+// Export the HamburgerMenu class for use in other parts of the app
 export default HamburgerMenu;
